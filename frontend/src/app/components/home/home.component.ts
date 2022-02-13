f5254cd7f751a470b6fd3020048db64526554403
anchal66 <anchalavinash66@gmail.com>
2022-02-13 18:49:53 +0530

    favicon change and card impl

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -10,9 +10,9 @@ export class HomeComponent implements OnInit {
   isMobile: boolean;
 
   images = [
-    { path: 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg', text1: '', text2: '' },
-    { path: 'https://images.ctfassets.net/hrltx12pl8hq/3MbF54EhWUhsXunc5Keueb/60774fbbff86e6bf6776f1e17a8016b4/04-nature_721703848.jpg?fit=fill&w=480&h=270', text1: '', text2: '' },
-    { path: 'https://media.istockphoto.com/photos/child-hands-formig-heart-shape-picture-id951945718?k=6&m=951945718&s=612x612&w=0&h=ih-N7RytxrTfhDyvyTQCA5q5xKoJToKSYgdsJ_mHrv0=', text1: '', text2: '' }
+    { path: 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg', text1: '', text2: 'Set up more material recovery facilities and recycling facilities' },
+    { path: 'https://images.ctfassets.net/hrltx12pl8hq/3MbF54EhWUhsXunc5Keueb/60774fbbff86e6bf6776f1e17a8016b4/04-nature_721703848.jpg?fit=fill&w=480&h=270', text1: '', text2: 'Formalisation of informal markets, enabling better recovery of recyclable materials through traceable collection and recycling chains' },
+    { path: 'https://media.istockphoto.com/photos/child-hands-formig-heart-shape-picture-id951945718?k=6&m=951945718&s=612x612&w=0&h=ih-N7RytxrTfhDyvyTQCA5q5xKoJToKSYgdsJ_mHrv0=', text1: '', text2: 'Engage with waste generators, bringing about behavioral changes like segregation at source' }
   ];
 
   facts = [
